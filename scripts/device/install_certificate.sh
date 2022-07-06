c831f25571041a39997a0026fe3a0e4123f4ddb8
MaksPetrukhnoPI <94057303+MaksPetrukhnoPI@users.noreply.github.com>
2022-07-06 11:27:00 +0200

    fixed sh scripts

--- scripts/device/install_certificate.sh
+++ scripts/device/install_certificate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# shellcheck disable=SC2006
-subjectHash=`openssl x509 -inform PEM -subject_hash_old -in androidBMPCertificate.pem | head -n 1`
-openssl x509 -in androidBMPCertificate.pem -inform PEM -outform DER -out $subjectHash.0
 adb root
-adb push ./$subjectHash.0 /data/misc/user/0/cacerts-added/$subjectHash.0
-adb shell "su 0 chmod 644 /data/misc/user/0/cacerts-added/$subjectHash.0"
-adb reboot
+certificate=`adb shell ls /data/misc/user/0/cacerts-added | grep 5bc1dc75.0`
+if [ -z "$certificate"  ]; then
+  subjectHash=`openssl x509 -inform PEM -subject_hash_old -in androidBMPCertificate.pem | head -n 1`
+  openssl x509 -in androidBMPCertificate.pem -inform PEM -outform DER -out $subjectHash.0
+  adb push ./$subjectHash.0 /data/misc/user/0/cacerts-added/$subjectHash.0
+  adb shell "su 0 chmod 644 /data/misc/user/0/cacerts-added/$subjectHash.0"
+  adb reboot
+fi
--- src/main/java/utils/AndroidEmulatorUtils.java
+++ src/main/java/utils/AndroidEmulatorUtils.java
@@ -1,13 +1,7 @@
 package utils;
 
-import com.google.common.hash.Hashing;
-
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class AndroidEmulatorUtils {
@@ -27,26 +21,16 @@ public class AndroidEmulatorUtils {
     }
 
     public static void installAndroidBmpCertificate() {
-        if (!isAndroidBmpCertificateInstalled()) {
-            CommandUtils.executeCommand("bash scripts/device/install_certificate.sh");
-        }
-    }
-
-    private static boolean isAndroidBmpCertificateInstalled() {
-        return getCertificates().contains("5bc1dc75.0");
+        CommandUtils.executeCommand("bash scripts/device/install_certificate.sh");
     }
 
     private static boolean isAndroidProxyEnabled() {
         String proxy = CommandUtils.executeCommand("adb shell settings get global http_proxy");
         return !proxy.equals("null") && !proxy.contains(":0");
     }
-    private static List<String> getCertificates(){
-        String certificates = CommandUtils.executeCommand("adb shell ls /data/misc/user/0/cacerts-added");
-        return Arrays.stream(certificates.split(System.lineSeparator())).collect(Collectors.toList());
-    }
+
 
     public static void main(String[] args) {
-        System.out.println(getCertificates());
-        System.out.println(isAndroidBmpCertificateInstalled());
+
     }
 }
